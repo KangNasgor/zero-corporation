7a09469c6c7c514ae0ed08b0aa6d1508d3191910
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2024-10-18 15:26:43 +0700

    added sidebar animation

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -8,7 +8,11 @@
     @vite('resources/css/app.css')
 </head>
 <body class="bg-slate-900">
-    <h1 class="text-white text-center">Berhasil Log In Kang! Mantap</h1>
+    <div class="flex flex-col gap-1 mt-5 ml-5 w-fit" onclick="openMenu('bar-1', 'bar-2', 'bar-3')">
+        <div class="h-1 w-6 bg-white transition" id="bar-1"></div>
+        <div class="h-1 w-6 bg-white transition" id="bar-2"></div>
+        <div class="h-1 w-6 bg-white transition" id="bar-3"></div>
+    </div>
     <a href="{{ route('logout.user') }}" class="w-fit block mx-auto">
         <div class="bg-slate-800 text-white p-5 rounded-md w-fit mt-10">
             Logout
@@ -16,3 +20,24 @@
     </a>
 </body>
 </html>
+<script>
+    let opened = false;
+    function openMenu(bar1Id, bar2Id, bar3Id){
+        const bar1 = document.getElementById(bar1Id);
+        const bar2 = document.getElementById(bar2Id);
+        const bar3 = document.getElementById(bar3Id);
+        if(opened == false){
+            bar1.style.transform = "translate(0, 8px) rotate(-45deg)";
+            bar2.style.opacity = "0";
+            bar3.style.transform = "translate(0, -8px) rotate(45deg)";
+            opened = true;
+        }
+        else if(opened == true){
+            bar1.style.transform = "translate(0, 0) rotate(0)";
+            bar2.style.opacity = "100";
+            bar3.style.transform = "translate(0, 0) rotate(0)";
+            opened = false;
+        }
+        
+    }
+</script>
