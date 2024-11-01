2d1ad630f4f8fc94edd6ebb9d97004e47b3a31de
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2024-11-01 10:32:47 +0700

    changed profile's color theme to purple

--- resources/views/user/profile/profile.blade.php
+++ resources/views/user/profile/profile.blade.php
@@ -15,8 +15,8 @@
                 <p class="text-white">Nama : {{ Auth::user()->name }}</p>
                 <p class="text-white">Email : {{ Auth::user()->email }}</p>
                 <i class="fa-solid fa-pen-to-square text-white cursor-pointer w-fit" id="edit-data" onclick="openEditMode()"></i>
-                <a href="{{ route('password.request') }}" class="inline-block">
-                    <button type="button" class="w-1/2 bg-[#FFD700] mt-2 rounded-md p-1 text-black text-sm">Change password</button>
+                <a href="{{ route('password.request') }}" class="w-fit">
+                    <button type="button" class="w-full bg-purple-950 mt-2 rounded-md p-2 text-white text-sm">Change password</button>
                 </a>
             </div>
             <form class="hidden grid-cols-2 gap-1" id="input-data" action="{{ route('profile.update', Auth::user()->id) }}" method="POST">
