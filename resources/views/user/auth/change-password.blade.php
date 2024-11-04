62405d64f92cacc99e1a62ae9f4660c53dea823f
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2024-11-04 09:21:00 +0700

    adding spaces to each element in change password page

--- resources/views/user/auth/change-password.blade.php
+++ resources/views/user/auth/change-password.blade.php
@@ -7,14 +7,14 @@
     <title>Reset password</title>
     @vite('resources/css/app.css')
 </head>
-<body class="bg-slate-900 flex justify-center items-center h-screen">
-    <form action="{{ route('password.email') }}" method="POST" class="bg-slate-700 p-10 rounded-md flex flex-col w-3/12">
+<body class="bg-black flex justify-center items-center h-screen">
+    <form action="{{ route('password.email') }}" method="POST" class="bg-gradient-to-br from-purple-900 to-purple-800 p-10 rounded-md flex flex-col w-3/12">
         @csrf
         @method('POST')
-        <h1 class="text-white">Please type your email so we can send you password reset link</h1>
-        <label class="text-white">Email</label>
-        <input type="email" placeholder="email" name="email" required class="px-2 rounded-md">
-        <button type="submit" class="w-1/2 bg-slate-800 mt-2 rounded-md p-3 text-white text-sm">Submit</button>
+        <h1 class="text-white mb-3">Please type your email so we can send you password reset link</h1>
+        <label class="text-white mb-2">Email</label>
+        <input type="email" placeholder="Email" name="email" required class="px-2 py-1 rounded-md mb-4">
+        <button type="submit" class="w-1/2 bg-purple-500 mt-2 rounded-md p-3 text-white text-sm">Submit</button>
     </form>
 </body>
 </html>
