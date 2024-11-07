21621c01e9f545e16bceccc8533bd1956c80da2d
KangNasgor <74401836+KangNasgor@users.noreply.github.com>
2024-11-07 15:35:32 +0700

    created first blog

--- app/Http/Controllers/User/BlogpageController.php
+++ app/Http/Controllers/User/BlogpageController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Blog;
 
 class BlogpageController extends Controller
 {
-    //
+    public function blogpage(){
+        $blog = Blog::where('status', 'active')->all();
+    }
 }
